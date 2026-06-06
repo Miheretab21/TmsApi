54db0d90a5bbc4ad2ad8586204e4869fef4d9f38
Miheretab21 <mereboom68@gmail.com>
2026-06-06 09:55:31 +0300

    correctly added TrainingAuthHandler.cs and edited program.cs to generate 401

--- Program.cs
+++ Program.cs
@@ -26,15 +26,8 @@
 // var builder = WebApplication.CreateBuilder(args);
 
 // builder.Services
-//     .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-//     .AddCookie(options =>
-//     {
-//         options.Events.OnRedirectToLogin = context =>
-//         {
-//             context.Response.StatusCode = 401;
-//             return Task.CompletedTask;
-//         };
-//     });
+//     .AddAuthentication("Training")
+//     .AddScheme<AuthenticationSchemeOptions, TrainingAuthHandler>("Training", null);
 
 // builder.Services.AddAuthorization();
 
@@ -57,22 +50,15 @@
 // we got 401.
 
 //exercise 1b
-
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Authentication
 builder.Services
-    .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-    .AddCookie(options =>
-    {
-        options.Events.OnRedirectToLogin = context =>
-        {
-            context.Response.StatusCode = 401;
-            return Task.CompletedTask;
-        };
-    });
+    .AddAuthentication("Training")
+    .AddScheme<AuthenticationSchemeOptions, TrainingAuthHandler>("Training", null);
 
 // Authorization
 builder.Services.AddAuthorization();
